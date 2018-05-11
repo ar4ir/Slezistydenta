21aa5a23f444fb4a787f1af94152267c0364b61e
IlyaOrange <36660991+IlyaOrange@users.noreply.github.com>
2018-05-11 08:24:11 +0500

    тесты

--- src/test/java/SalaryCalc/SalaryCalcTest.java
+++ src/test/java/SalaryCalc/SalaryCalcTest.java
@@ -37,7 +37,27 @@ class SalaryCalcTest {
         testingClass.arrTf[4].setText("88");
         testingClass.calcSalary();
         assertEquals("1720.49", testingClass.arrTf[9].getText());
-} @Test
+	}
+		@Test
+    void calcSalary5() throws Exception {
+        testingClass.arrTf[1].setText("100");
+        testingClass.arrTf[2].setText("9");
+        testingClass.arrTf[3].setText("24");
+        testingClass.arrTf[4].setText("13");
+        testingClass.calcSalary();
+        assertEquals("20671.20", testingClass.arrTf[9].getText());
+} 
+@Test
+    void calcSalary6() throws Exception {
+        testingClass.arrTf[1].setText("110");
+        testingClass.arrTf[2].setText("11");
+        testingClass.arrTf[3].setText("31");
+        testingClass.arrTf[4].setText("13");
+        testingClass.calcSalary();
+        assertEquals("35897.07", testingClass.arrTf[9].getText());
+
+}
+ @Test
     void calcSalary7() throws Exception {
         testingClass.arrTf[1].setText("320");
         testingClass.arrTf[2].setText("8");
