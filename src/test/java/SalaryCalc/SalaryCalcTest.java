7d24d4100f5fd265f5c77bdb2af9681f803ac9b5
ar4ir <37943670+ar4ir@users.noreply.github.com>
2018-05-13 20:40:30 +0500

    Update SalaryCalcTest.java

--- src/test/java/SalaryCalc/SalaryCalcTest.java
+++ src/test/java/SalaryCalc/SalaryCalcTest.java
@@ -21,7 +21,7 @@ class SalaryCalcTest {
     }
 
     @Test
-    void calcSalary1() throws Exception {
+    void calcSalary() throws Exception {
         testingClass.arrTf[1].setText("228");
         testingClass.arrTf[2].setText("5");
         testingClass.arrTf[3].setText("13");
@@ -37,7 +37,8 @@ class SalaryCalcTest {
         testingClass.arrTf[4].setText("88");
         testingClass.calcSalary();
         assertEquals("1720.49", testingClass.arrTf[9].getText());
-	}	
+} 
+
  @Test
     void calcSalary3() throws Exception {
         testingClass.arrTf[1].setText("240");
@@ -56,28 +57,26 @@ class SalaryCalcTest {
         testingClass.arrTf[4].setText("13");
         testingClass.calcSalary();
         assertEquals("79316.16", testingClass.arrTf[9].getText());
-
-}
-	@Test
-+    void calcSalary5() throws Exception {
-+        testingClass.arrTf[1].setText("100");
-+        testingClass.arrTf[2].setText("9");
-+        testingClass.arrTf[3].setText("24");
-+        testingClass.arrTf[4].setText("13");
-+        testingClass.calcSalary();
-+        assertEquals("20671.20", testingClass.arrTf[9].getText());
-+} 
-+@Test
-+    void calcSalary6() throws Exception {
-+        testingClass.arrTf[1].setText("110");
-+        testingClass.arrTf[2].setText("11");
-+        testingClass.arrTf[3].setText("31");
-+        testingClass.arrTf[4].setText("13");
-+        testingClass.calcSalary();
-+        assertEquals("35897.07", testingClass.arrTf[9].getText());
-+
-+}
- @Test
+} 
+@Test
+    void calcSalary5() throws Exception {
+        testingClass.arrTf[1].setText("100");
+        testingClass.arrTf[2].setText("9");
+        testingClass.arrTf[3].setText("24");
+        testingClass.arrTf[4].setText("13");
+        testingClass.calcSalary();
+        assertEquals("20671.20", testingClass.arrTf[9].getText());
+} 
+@Test
+    void calcSalary6() throws Exception {
+        testingClass.arrTf[1].setText("110");
+        testingClass.arrTf[2].setText("11");
+        testingClass.arrTf[3].setText("31");
+        testingClass.arrTf[4].setText("13");
+        testingClass.calcSalary();
+        assertEquals("35897.07", testingClass.arrTf[9].getText());
+} 
+@Test
     void calcSalary7() throws Exception {
         testingClass.arrTf[1].setText("320");
         testingClass.arrTf[2].setText("8");
